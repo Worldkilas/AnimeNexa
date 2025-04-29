6f5ef02174369aafff79a6023709221011ac19cd
Mustapha Amin <mustaphaamin2003@gmail.com>
2025-04-29 09:44:36 +0100

    fix plus icon color and remaining nav bar items

--- lib/shared/view/layout_scaffold.dart
+++ lib/shared/view/layout_scaffold.dart
@@ -1,3 +1,5 @@
+import 'package:anime_nexa/shared/constants/app_theme.dart';
+import 'package:anime_nexa/shared/constants/app_typography.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
@@ -11,30 +13,62 @@ class LayoutScaffold extends StatelessWidget {
     return Scaffold(
       body: statefulNavigationShell,
       bottomNavigationBar: NavigationBar(
+          backgroundColor: Colors.white,
+          surfaceTintColor: null,
+          labelTextStyle: WidgetStateTextStyle.resolveWith((states) {
+            if (states.contains(WidgetState.selected)) {
+              return AppTypography.textXSmall
+                  .copyWith(color: appTheme.primaryColor);
+            }
+            return AppTypography.textXSmall.copyWith(color: Colors.grey);
+          }),
           selectedIndex: statefulNavigationShell.currentIndex,
-          onDestinationSelected: (index) => index == 2 ? context.push('/post') : statefulNavigationShell.goBranch(index),
-          indicatorColor: Theme.of(context).primaryColor,
-          // labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          onDestinationSelected: (index) => index == 2
+              ? context.push('/post')
+              : statefulNavigationShell.goBranch(index),
+          indicatorColor: Colors.transparent,
           destinations: [
             NavigationDestination(
               icon: SvgPicture.asset('lib/assets/icons/home.svg'),
               label: 'Home',
+              selectedIcon: SvgPicture.asset(
+                'lib/assets/icons/home.svg',
+                colorFilter:
+                    ColorFilter.mode(appTheme.primaryColor, BlendMode.srcIn),
+              ),
             ),
             NavigationDestination(
               icon: SvgPicture.asset('lib/assets/icons/discover.svg'),
               label: 'Discover',
+              selectedIcon: SvgPicture.asset(
+                'lib/assets/icons/discover.svg',
+                colorFilter:
+                    ColorFilter.mode(appTheme.primaryColor, BlendMode.srcIn),
+              ),
             ),
             NavigationDestination(
-              icon: SvgPicture.asset('lib/assets/icons/Add.svg'),
+              icon: SvgPicture.asset(
+                'lib/assets/icons/Add.svg',
+              ),
               label: '',
             ),
             NavigationDestination(
               icon: SvgPicture.asset('lib/assets/icons/messages.svg'),
               label: 'Messages',
+              selectedIcon: SvgPicture.asset(
+                'lib/assets/icons/messages.svg',
+                colorFilter:
+                    ColorFilter.mode(appTheme.primaryColor, BlendMode.srcIn),
+              ),
             ),
             NavigationDestination(
               icon: SvgPicture.asset('lib/assets/icons/profile.svg'),
               label: 'Profile',
+              selectedIcon: SvgPicture.asset(
+                'lib/assets/icons/profile.svg',
+                colorFilter:
+                    ColorFilter.mode(appTheme.primaryColor, BlendMode.srcIn),
+              ),
             ),
           ]),
     );
